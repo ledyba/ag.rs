e31256ce4ade185f6c33afcaf19aa4705f8a612c
Kaede Fujisaki <kaede@hexe.net>
2022-01-20 16:23:44 +0900

    add fetch functions

--- src/tiff/stream.rs
+++ src/tiff/stream.rs
@@ -9,7 +9,7 @@ pub struct Stream {
 }
 
 impl Stream {
-  pub fn open(path: &str) -> anyhow::Result<Stream> {
+  pub fn open(path: &str) -> std::io::Result<Stream> {
     let mut file = std::fs::File::open(path)?;
     let endian = {
       let mut header: [u8; 2] = [0, 0];
@@ -19,7 +19,7 @@ impl Stream {
       } else if header == [0x49, 0x49] {
         Endianness::Little
       } else {
-        return Err(anyhow::Error::msg("Not a TIFF file."));
+        return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "Not a TIFF file."));
       }
     };
     file.seek(SeekFrom::Start(0))?;
@@ -33,88 +33,136 @@ impl Stream {
   pub fn read_u8(&mut self) -> std::io::Result<u8> {
     self.file.read_u8()
   }
-  pub fn read_u8s(&mut self, n: usize) -> std::io::Result<Vec<u8>> {
+  pub fn fetch_u8(&mut self, offset: u64) -> std::io::Result<u8> {
+    self.warp(offset, |s| s.read_u8())
+  }
+  pub fn read_vec_u8(&mut self, n: usize) -> std::io::Result<Vec<u8>> {
     let mut buff = Vec::<u8>::new();
     buff.resize(n, 0);
     self.file.read_exact(&mut buff)?;
     Ok(buff)
   }
+  pub fn fetch_vec_u8(&mut self, offset: u64, n: usize) -> std::io::Result<Vec<u8>> {
+    self.warp(offset, |s| s.read_vec_u8(n))
+  }
 
-  /* u8 */
+  /* i8 */
   pub fn read_i8(&mut self) -> std::io::Result<i8> {
     self.file.read_i8()
   }
-  pub fn read_i8s(&mut self, n: usize) -> std::io::Result<Vec<i8>> {
+  pub fn fetch_i8(&mut self, offset: u64) -> std::io::Result<i8> {
+    self.warp(offset, |s| s.read_i8())
+  }
+  pub fn read_vec_i8(&mut self, n: usize) -> std::io::Result<Vec<i8>> {
     let mut buff = Vec::<i8>::new();
     buff.resize(n, 0);
     self.file.read_i8_into(&mut buff)?;
     Ok(buff)
+  }
+  pub fn fetch_vec_i8(&mut self, offset: u64, n: usize) -> std::io::Result<Vec<i8>> {
+    self.warp(offset, |s| s.read_vec_i8(n))
   }
 
   /* u16 */
   pub fn read_u16(&mut self) -> std::io::Result<u16> {
     self.endian.read_u16(&mut self.file)
   }
-  pub fn read_u16s(&mut self, n: usize) -> std::io::Result<Vec<u16>> {
+  pub fn fetch_u16(&mut self, offset: u64) -> std::io::Result<u16> {
+    self.warp(offset, |s| s.endian.read_u16(&mut s.file))
+  }
+  pub fn read_vec_u16(&mut self, n: usize) -> std::io::Result<Vec<u16>> {
     let mut buff: Vec<u16> = Vec::new();
     buff.resize(n, 0);
     self.endian.read_u16_into(&mut self.file, &mut buff)?;
     Ok(buff)
+  }
+  pub fn fetch_vec_u16(&mut self, offset: u64, n: usize) -> std::io::Result<Vec<u16>> {
+    self.warp(offset, |s| s.read_vec_u16(n))
   }
 
   /* s16 */
   pub fn read_i16(&mut self) -> std::io::Result<i16> {
     self.endian.read_i16(&mut self.file)
   }
-  pub fn read_i16s(&mut self, n: usize) -> std::io::Result<Vec<i16>> {
+  pub fn fetch_i16(&mut self, offset: u64) -> std::io::Result<i16> {
+    self.warp(offset, |s| s.read_i16())
+  }
+  pub fn read_vec_i16(&mut self, n: usize) -> std::io::Result<Vec<i16>> {
     let mut buff: Vec<i16> = Vec::new();
     buff.resize(n, 0);
     self.endian.read_i16_into(&mut self.file, &mut buff)?;
     Ok(buff)
+  }
+  pub fn fetch_vec_i16(&mut self, offset: u64, n: usize) -> std::io::Result<Vec<i16>> {
+    self.warp(offset, |s| s.read_vec_i16(n))
   }
 
   /* u32 */
   pub fn read_u32(&mut self) -> std::io::Result<u32> {
     self.endian.read_u32(&mut self.file)
   }
-  pub fn read_u32s(&mut self, n: usize) -> std::io::Result<Vec<u32>> {
+  pub fn fetch_u32(&mut self, offset: u64) -> std::io::Result<u32> {
+    self.warp(offset, |s| s.read_u32())
+  }
+  pub fn read_vec_u32(&mut self, n: usize) -> std::io::Result<Vec<u32>> {
     let mut buff: Vec<u32> = Vec::new();
     buff.resize(n, 0);
     self.endian.read_u32_into(&mut self.file, &mut buff)?;
     Ok(buff)
+  }
+  pub fn fetch_vec_u32(&mut self, offset: u64, n: usize) -> std::io::Result<Vec<u32>> {
+    self.warp(offset, |s| s.read_vec_u32(n))
   }
 
   /* s32 */
   pub fn read_i32(&mut self) -> std::io::Result<i32> {
     self.endian.read_i32(&mut self.file)
   }
-  pub fn read_i32s(&mut self, n: usize) -> std::io::Result<Vec<i32>> {
+  pub fn fetch_i32(&mut self, offset: u64) -> std::io::Result<i32> {
+    self.warp(offset, |s| s.read_i32())
+  }
+  pub fn read_vec_i32(&mut self, n: usize) -> std::io::Result<Vec<i32>> {
     let mut buff: Vec<i32> = Vec::new();
     buff.resize(n, 0);
     self.endian.read_i32_into(&mut self.file, &mut buff)?;
     Ok(buff)
+  }
+  pub fn fetch_vec_i32(&mut self, offset: u64, n: usize) -> std::io::Result<Vec<i32>> {
+    self.warp(offset, |s| s.read_vec_i32(n))
   }
 
   /* f32 */
   pub fn read_f32(&mut self) -> std::io::Result<f32> {
     self.endian.read_f32(&mut self.file)
   }
-  pub fn read_f32s(&mut self, n: usize) -> std::io::Result<Vec<f32>> {
+  pub fn fetch_f32(&mut self, offset: u64) -> std::io::Result<f32> {
+    self.warp(offset, |s| s.read_f32())
+  }
+  pub fn read_vec_f32(&mut self, n: usize) -> std::io::Result<Vec<f32>> {
     let mut buff: Vec<f32> = Vec::new();
     buff.resize(n, 0.0);
     self.endian.read_f32_into(&mut self.file, &mut buff)?;
     Ok(buff)
+  }
+  pub fn fetch_vec_f32(&mut self, offset: u64, n: usize) -> std::io::Result<Vec<f32>> {
+    self.warp(offset, |s| s.read_vec_f32(n))
   }
 
   /* f64 */
   pub fn read_f64(&mut self) -> std::io::Result<f64> {
     self.endian.read_f64(&mut self.file)
   }
-  pub fn read_f64s(&mut self, n: usize) -> std::io::Result<Vec<f64>> {
+  pub fn fetch_f64(&mut self, offset: u64) -> std::io::Result<f64> {
+    self.warp(offset, |s| s.read_f64())
+  }
+  pub fn read_vec_f64(&mut self, n: usize) -> std::io::Result<Vec<f64>> {
     let mut buff: Vec<f64> = Vec::new();
     buff.resize(n, 0.0);
     self.endian.read_f64_into(&mut self.file, &mut buff)?;
     Ok(buff)
+  }
+  pub fn fetch_vec_f64(&mut self, offset: u64, n: usize) -> std::io::Result<Vec<f64>> {
+    self.warp(offset, |s| s.read_vec_f64(n))
   }
 
   /* rational */
@@ -126,38 +174,43 @@ impl Stream {
       denominator: buff[1],
     })
   }
-  pub fn read_unsigned_rationals(&mut self, n: usize) -> std::io::Result<Vec<UnsignedRational>> {
-    let buff = self.read_u32s(n * 2)?;
-    let values: Vec<UnsignedRational> = buff.chunks(2).map(|v| UnsignedRational {
+  pub fn fetch_unsigned_rational(&mut self, offset: u64) -> std::io::Result<UnsignedRational> {
+    self.warp(offset, |s| s.read_unsigned_rational())
+  }
+  pub fn read_vec_unsigned_rational(&mut self, n: usize) -> std::io::Result<Vec<UnsignedRational>> {
+    let buff = self.read_vec_u32(n * 2)?;
+    let values: Vec<UnsignedRational> = buff.chunks_exact(2).map(|v| UnsignedRational {
       numerator: v[0],
       denominator: v[1],
     }).collect();
     Ok(values)
   }
+  pub fn fetch_unsigned_rationals(&mut self, offset: u64, n: usize) -> std::io::Result<Vec<UnsignedRational>> {
+    self.warp(offset, |s| s.read_vec_unsigned_rational(n))
+  }
 
   /* SRational */
-  pub fn read_signed_rational(&mut self) -> anyhow::Result<SignedRational> {
+  pub fn read_signed_rational(&mut self) -> std::io::Result<SignedRational> {
     let mut buff: [i32; 2] = [0, 0];
-    s.endian.read_i32_into(&mut s.file, &mut buff)?;
+    self.endian.read_i32_into(&mut self.file, &mut buff)?;
     Ok(SignedRational {
       numerator: buff[0],
       denominator: buff[1],
     })
   }
-  pub fn fetch_signed_rational(&mut self, offset: u64) -> anyhow::Result<SignedRational> {
-    self.warp(offset, |s| {
-      s.read_signed_rational()
-    })
+  pub fn fetch_signed_rational(&mut self, offset: u64) -> std::io::Result<SignedRational> {
+    self.warp(offset, |s| s.read_signed_rational())
   }
-  pub fn fetch_signed_rationals(&mut self, offset: u64, n: usize) -> anyhow::Result<Vec<SignedRational>> {
-    self.warp(offset, |s| {
-      let buff = s.read_i32s(n * 2)?;
-      let values: Vec<SignedRational> = buff.chunks_exact(2).map(|v| SignedRational {
-        numerator: v[0],
-        denominator: v[1],
-      }).collect();
-      Ok(values)
-    })
+  pub fn read_vec_signed_rational(&mut self, n: usize) -> std::io::Result<Vec<SignedRational>> {
+    let buff = self.read_vec_i32(n * 2)?;
+    let values: Vec<SignedRational> = buff.chunks_exact(2).map(|v| SignedRational {
+      numerator: v[0],
+      denominator: v[1],
+    }).collect();
+    Ok(values)
+  }
+  pub fn fetch_signed_rationals(&mut self, offset: u64, n: usize) -> std::io::Result<Vec<SignedRational>> {
+    self.warp(offset, |s| s.read_vec_signed_rational(n))
   }
 
   /* Skip */
@@ -175,9 +228,10 @@ impl Stream {
     self.file.stream_position()
   }
 
-  pub fn warp<'s, Fn, T>(&'s mut self, offset: u64, f: Fn) -> anyhow::Result<T>
+  fn warp<'s, Fn, T>(&'s mut self, offset: u64, f: Fn) -> std::io::Result<T>
     where
-    for<'f> Fn: FnOnce(&'f mut Self) -> anyhow::Result<T>,
+    // https://doc.rust-lang.org/reference/trait-bounds.html#higher-ranked-trait-bounds
+    for<'f> Fn: FnOnce(&'f mut Self) -> std::io::Result<T>,
   {
     self.fork(|stream| {
       stream.seek(offset)?;
@@ -185,10 +239,9 @@ impl Stream {
     })
   }
 
-  pub fn fork<'s, Fn, T>(&'s mut self, f: Fn) -> anyhow::Result<T>
+  fn fork<'s, Fn, T>(&'s mut self, f: Fn) -> std::io::Result<T>
     where
-    // https://doc.rust-lang.org/reference/trait-bounds.html#higher-ranked-trait-bounds
-    for <'f> Fn: FnOnce(&'f mut Self) -> anyhow::Result<T>,
+    for <'f> Fn: FnOnce(&'f mut Self) -> std::io::Result<T>,
   {
     let original_offset = self.position()?;
     let r = f(self);
