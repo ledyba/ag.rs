5750b0a4b870675e552a7cc79a43846530c619ae
Kaede Fujisaki <kaede@hexe.net>
2022-05-09 15:49:07 +0900

    add ref

--- src/raw/decompressor/arw/arw2.rs
+++ src/raw/decompressor/arw/arw2.rs
@@ -1,9 +1,12 @@
 /*
 # Reference
 
-- rawspeed
-  - https://github.com/darktable-org/rawspeed/blob/088ad10ca9c3064f0af892e87d50d5ca24481205/src/librawspeed/decompressors/SonyArw2Decompressor.h
-  - https://github.com/darktable-org/rawspeed/blob/088ad10ca9c3064f0af892e87d50d5ca24481205/src/librawspeed/decompressors/SonyArw2Decompressor.cpp
+libraw
+- https://github.com/LibRaw/LibRaw/blob/2a9a4de21ea7f5d15314da8ee5f27feebf239655/src/decoders/decoders_dcraw.cpp#L1455
+
+rawspeed
+- https://github.com/darktable-org/rawspeed/blob/088ad10ca9c3064f0af892e87d50d5ca24481205/src/librawspeed/decompressors/SonyArw2Decompressor.h
+- https://github.com/darktable-org/rawspeed/blob/088ad10ca9c3064f0af892e87d50d5ca24481205/src/librawspeed/decompressors/SonyArw2Decompressor.cpp
 
 */
 
